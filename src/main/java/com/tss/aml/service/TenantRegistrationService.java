d01f55b4f1557adc9699d509d5180524e60d97f1
Neel.Chhantbar <neel.chhantbar@tssconsultancy.com>
2026-04-23 19:16:00 +0530

    chore: unused imports removed

--- src/main/java/com/tss/aml/service/TenantRegistrationService.java
+++ src/main/java/com/tss/aml/service/TenantRegistrationService.java
@@ -1,17 +1,9 @@
 package com.tss.aml.service;
 
-import com.tss.aml.context.TenantContext;
 import com.tss.aml.dto.request.RegisterRequestDto;
-import com.tss.aml.enums.TenantStatus;
-import com.tss.aml.master.entity.Tenant;
 import com.tss.aml.master.repository.TenantRepository;
 import lombok.RequiredArgsConstructor;
-import org.flywaydb.core.Flyway;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.sql.DataSource;
 
 @Service
 @RequiredArgsConstructor
