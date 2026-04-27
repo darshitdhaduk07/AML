ad48fa344090c115d7933b2620eb031e20912ab1
Darshit Dhaduk <darshitdhaduk129@gmail.com>
2026-04-27 17:13:52 +0530

    bug: complete global excepiton and file related issue

--- src/main/java/com/tss/aml/tenant/repository/CustomerRepository.java
+++ src/main/java/com/tss/aml/tenant/repository/CustomerRepository.java
@@ -2,10 +2,15 @@ package com.tss.aml.tenant.repository;
 
 import com.tss.aml.tenant.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     boolean existsByCustomerNumber(String customerNumber);
 
+    @Query("SELECT c.customerNumber FROM Customer c WHERE c.customerNumber IN :customerNumbers")
+    Set<String> findExistingCustomerNumbers(@Param("customerNumbers") Set<String> customerNumbers);
 }
